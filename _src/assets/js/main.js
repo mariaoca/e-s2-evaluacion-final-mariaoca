b10d2241b40233fd11adf912729535f63576c708
mariaoca <mariaolidcamarena@gmail.com>
2018-12-17 19:53:26 +0100

    dom and favorite item

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -4,40 +4,47 @@ const search = document.querySelector('#search');
 const btnSearch = document.querySelector('.btn_search');
 const dates = document.querySelector('.dates_list');
 const apiUrl = `http://api.tvmaze.com/search/shows?q=`;
-const seriesList = document.querySelectorAll('.list_series');
-//Recoger texto de búsqueda en el input
-//Buscar en la API la búsqueda
-//mostrar resultados con foto y título
-function takeName() {
-    // dates.innerHTML = '';
+
+function takeName(e) {
+    e.preventDefault()
+    dates.innerHTML = '';
     const name = search.value;
+    console.log(name, 'hola');
     fetch(apiUrl + name)
         .then(response => response.json())
         .then(data => {
-            let items = '';
             for (let i = 0; i < data.length; i++) {
-                const nameSeries = data[i].show.name;
-                const imageSeries = data[i].show.image;
-                const imgNull = `https://via.placeholder.com/210x295/cccccc/666666/?text=TV`;
-                if (imageSeries === null) {
-                    items += `<li class="list_series_null">
-                    <image src = "${imgNull}" class="img">
-                    <h2 class="title_series">${nameSeries}</h2>
-                    </li>`;
+
+                const list = document.createElement('li');
+                list.classList.add('list_series');
+
+
+                const img = document.createElement('img');
+               
+
+                const title = document.createElement('h2');
+                const nameSeries = document.createTextNode(data[i].show.name);
+                title.classList.add('title_series');
+                title.appendChild(nameSeries);
+
+             
+
+                if ( data[i].show.image=== null) {
+                    img.src = `https://via.placeholder.com/210x295/cccccc/666666/?text=TV`;
                 } else {
-                    items += `<li class="list_series"> 
-                    <image src ="${imageSeries.medium}" class="img">
-                    <h2 class ="title_series">${nameSeries}</h2>
-                    </li>`;
+                    img.src = data[i].show.image.medium;
                 }
+
+                list.appendChild(img);
+                list.appendChild(title);
+
+                dates.appendChild(list);
+                list.addEventListener('click',addFavorite);
             }
-            dates.innerHTML = items;
-
-        })
+        });
 }
-btnSearch.addEventListener('click', takeName);
-
-
-//marcar como favorito
-//guardar en LocalStorage los favoritos
-
+function addFavorite(e) {
+    const seriesList = e.currentTarget;
+    seriesList.classList.toggle('favorite_item');
+}
+btnSearch.addEventListener('click', takeName);        
